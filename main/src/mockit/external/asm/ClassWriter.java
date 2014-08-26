35f04b6daf1f8350f488ad791339dd09e3bb5c5f
rliesenfeld <rliesenfeld@gmail.com>
2014-08-25 22:43:04 -0300

    Workaround to avoid duplicate class definition when ASM reconstructs stackmap frames. Closes #50.

--- main/src/mockit/external/asm/ClassWriter.java
+++ main/src/mockit/external/asm/ClassWriter.java
@@ -1675,28 +1675,8 @@ public final class ClassWriter extends ClassVisitor {
      *         classes.
      */
     protected String getCommonSuperClass(final String type1, final String type2) {
-        Class<?> c, d;
-        ClassLoader classLoader = getClass().getClassLoader();
-        try {
-            c = Class.forName(type1.replace('/', '.'), false, classLoader);
-            d = Class.forName(type2.replace('/', '.'), false, classLoader);
-        } catch (Exception e) {
-            throw new RuntimeException(e.toString());
-        }
-        if (c.isAssignableFrom(d)) {
-            return type1;
-        }
-        if (d.isAssignableFrom(c)) {
-            return type2;
-        }
-        if (c.isInterface() || d.isInterface()) {
-            return "java/lang/Object";
-        } else {
-            do {
-                c = c.getSuperclass();
-            } while (!c.isAssignableFrom(d));
-            return c.getName().replace('.', '/');
-        }
+        // Replaced with this fixed return to avoid "duplicate class definition" errors.
+        return "java/lang/Object";
     }
 
     /**
