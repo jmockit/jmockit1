d8361bb073a6b28e99ec10a1708b47e07c3f547c
rliesenfeld <rliesenfeld@gmail.com>
2014-11-15 13:55:50 -0200

    Minor updates to "mockit" package API documentation.

--- main/src/mockit/package-info.java
+++ main/src/mockit/package-info.java
@@ -8,15 +8,15 @@
  * <p/>
  * The {@link mockit.Expectations} class provides an API for the traditional <em>record-replay</em> model of recording
  * expected invocations which are later replayed and implicitly verified.
- * This API makes use of the {@link mockit.Mocked} annotation.
+ * This API makes use of the {@linkplain mockit.Mocked @Mocked} annotation.
  * The {@link mockit.Verifications} class extends the record-replay model to a <em>record-replay-verify</em> model,
- * where the record phase can be left empty, with expected invocations verified explicitly <em>after</em> exercising the
- * code under test (ie, after the replay phase).
+ * where expectations that were not recorded can be verified explicitly <em>after</em> exercising the code under test
+ * (ie, after the replay phase).
  * <p/>
  * The {@linkplain mockit.MockUp <code>MockUp&lt;T></code>} generic class (where {@code T} is the mocked type) allows
  * the definition of fake implementations for arbitrary classes.
  * <p/>
- * For a description with examples of the <em>Expectations & Verifications</em> API, see the
+ * For a description with examples of the <em>Expectations</em> API, see the
  * "<a href="http://jmockit.github.io/tutorial/BehaviorBasedTesting.html">Writing behavior-based tests</a>" chapter in
  * the Tutorial.
  * For the <em>Mockups</em> API, see the "<a href="http://jmockit.github.io/tutorial/StateBasedTesting.html">Writing
