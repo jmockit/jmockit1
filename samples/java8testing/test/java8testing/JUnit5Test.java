350535e7562bb0f55662611243eea68246a3a528
rliesenfeld <rliesenfeld@gmail.com>
2016-04-22 14:04:16 -0300

    Added other JUnit 5 dependencies to allow test execution from the IDE.

--- samples/java8testing/test/java8testing/JUnit5Test.java
+++ samples/java8testing/test/java8testing/JUnit5Test.java
@@ -5,13 +5,26 @@
 package java8testing;
 
 import org.junit.gen5.api.*;
+import org.junit.gen5.api.Test;
+import org.junit.gen5.junit4.runner.*;
+import org.junit.runner.*;
 import static org.junit.gen5.api.Assertions.*;
 
-final class JUnit5Test
+import mockit.*;
+
+@RunWith(JUnit5.class)
+public final class JUnit5Test
 {
    @Test
    void basicExample(TestInfo testInfo)
    {
       assertNotNull(testInfo);
+      System.out.println(testInfo.getDisplayName());
+   }
+
+//   @Test
+   void testWithMockedParameter(@Mocked Runnable mock)
+   {
+      assertNull(mock);
    }
 }
