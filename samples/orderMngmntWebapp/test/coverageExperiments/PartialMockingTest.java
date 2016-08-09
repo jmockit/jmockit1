1fdb3ecdf6d19a100edf1b573cae6d75ad99effd
rliesenfeld <rliesenfeld@gmail.com>
2016-08-09 00:06:19 -0300

    Removed another old test.

--- samples/orderMngmntWebapp/test/coverageExperiments/PartialMockingTest.java
+++ samples/orderMngmntWebapp/test/coverageExperiments/PartialMockingTest.java
@@ -11,27 +11,17 @@ import mockit.*;
 
 public final class PartialMockingTest
 {
+   @Tested TestedClassPartiallyMocked sut;
+
    @Test
    public void useDynamicPartialMocking()
    {
-      final TestedClassPartiallyMocked sut = new TestedClassPartiallyMocked();
-
       new Expectations(sut) {{
          sut.doSomethingElse(); result = 5;
       }};
 
       int r = sut.doSomething("testing");
+
       assertEquals(12, r);
-   }
-
-   @Test
-   public void useAMockUp()
-   {
-      new MockUp<TestedClassPartiallyMocked>() {
-         @Mock int doSomethingElse() { return 8; }
-      };
-
-      int r = new TestedClassPartiallyMocked().doSomething("testing");
-      assertEquals(15, r);
    }
 }
