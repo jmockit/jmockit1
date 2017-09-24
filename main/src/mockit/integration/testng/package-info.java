8b6983390e847aca6e10ed49ff3d06658801421a
rliesenfeld <rliesenfeld@gmail.com>
2017-09-23 23:23:27 -0300

    Documentation update for testng package.

--- main/src/mockit/integration/testng/package-info.java
+++ main/src/mockit/integration/testng/package-info.java
@@ -8,12 +8,12 @@
  * This integration provides the following benefits to test code:
  * <ol>
  * <li>
- * Expected invocations specified through the Expectations API are automatically verified before the execution of a test
- * is completed.
+ * Expected invocations specified through the Mocking API are automatically verified before the execution of a test is
+ * completed.
  * </li>
  * <li>
- * Mock-up classes applied with the Mockups API from inside a method annotated as a {@code @Test} or a
- * {@code @BeforeMethod} will be discarded right after the execution of the test method or the whole test, respectively.
+ * Fake classes applied with the Faking API from inside a method annotated as a {@code @Test} or a {@code @BeforeMethod}
+ * will be discarded right after the execution of the test method or the whole test, respectively.
  * </li>
  * <li>
  * Test methods accept <em>mock parameters</em>, whose values are mocked instances automatically created by JMockit and
