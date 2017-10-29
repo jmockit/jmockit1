672f40727ff9dc0cdea75031e3116fc359ca4f5a
rliesenfeld <rliesenfeld@gmail.com>
2017-10-28 22:19:38 -0200

    Changes in comments only.

--- main/src/mockit/external/asm/AnnotationVisitor.java
+++ main/src/mockit/external/asm/AnnotationVisitor.java
@@ -30,9 +30,8 @@
 package mockit.external.asm;
 
 /**
- * A visitor to visit a Java annotation. The methods of this class must be
- * called in the following order: ( <tt>visit</tt> | <tt>visitEnum</tt> |
- * <tt>visitAnnotation</tt> | <tt>visitArray</tt> )* <tt>visitEnd</tt>.
+ * A visitor to visit a Java annotation. The methods of this class must be called in the following order:
+ * ( <tt>visit</tt> | <tt>visitEnum</tt> | <tt>visitAnnotation</tt> | <tt>visitArray</tt> )* <tt>visitEnd</tt>.
  * 
  * @author Eric Bruneton
  * @author Eugene Kuleshov
@@ -47,60 +46,47 @@ public abstract class AnnotationVisitor
     /**
      * Visits a primitive value of the annotation.
      * 
-     * @param name
-     *            the value name.
-     * @param value
-     *            the actual value, whose type must be {@link Byte},
-     *            {@link Boolean}, {@link Character}, {@link Short},
-     *            {@link Integer} , {@link Long}, {@link Float}, {@link Double},
-     *            {@link String} or {@link Type} or OBJECT or ARRAY sort. This
-     *            value can also be an array of byte, boolean, short, char, int,
-     *            long, float or double values (this is equivalent to using
-     *            {@link #visitArray visitArray} and visiting each array element
-     *            in turn, but is more convenient).
+     * @param name the value name.
+     * @param value the actual value, whose type must be {@link Byte}, {@link Boolean}, {@link Character},
+     *    {@link Short}, {@link Integer}, {@link Long}, {@link Float}, {@link Double}, {@link String} or
+     *    {@link Type} or OBJECT or ARRAY sort.
+     *    This value can also be an array of byte, boolean, short, char, int, long, float or double values (this is
+     *    equivalent to using {@link #visitArray visitArray} and visiting each array element in turn, but is more
+     *    convenient).
      */
     public void visit(String name, Object value) {}
 
     /**
      * Visits an enumeration value of the annotation.
      * 
-     * @param name
-     *            the value name.
-     * @param desc
-     *            the class descriptor of the enumeration class.
-     * @param value
-     *            the actual enumeration value.
+     * @param name the value name.
+     * @param desc the class descriptor of the enumeration class.
+     * @param value the actual enumeration value.
      */
     public void visitEnum(String name, String desc, String value) {}
 
     /**
      * Visits a nested annotation value of the annotation.
      * 
-     * @param name
-     *            the value name.
-     * @param desc
-     *            the class descriptor of the nested annotation class.
-     * @return a visitor to visit the actual nested annotation value, or
-     *         <tt>null</tt> if this visitor is not interested in visiting this
-     *         nested annotation. <i>The nested annotation value must be fully
-     *         visited before calling other methods on this annotation
-     *         visitor</i>.
+     * @param name the value name.
+     * @param desc the class descriptor of the nested annotation class.
+     *
+     * @return a visitor to visit the actual nested annotation value, or <tt>null</tt> if this visitor is not interested
+     *         in visiting this nested annotation. <i>The nested annotation value must be fully visited before calling
+     *         other methods on this annotation visitor</i>.
      */
     public AnnotationVisitor visitAnnotation(String name, String desc) { return null; }
 
     /**
-     * Visits an array value of the annotation. Note that arrays of primitive
-     * types (such as byte, boolean, short, char, int, long, float or double)
-     * can be passed as value to {@link #visit visit}. This is what
-     * {@link ClassReader} does.
+     * Visits an array value of the annotation. Note that arrays of primitive types (such as byte, boolean, short, char,
+     * int, long, float or double) can be passed as value to {@link #visit visit}.
+     * This is what {@link ClassReader} does.
      * 
-     * @param name
-     *            the value name.
-     * @return a visitor to visit the actual array value elements, or
-     *         <tt>null</tt> if this visitor is not interested in visiting these
-     *         values. The 'name' parameters passed to the methods of this
-     *         visitor are ignored. <i>All the array values must be visited
-     *         before calling other methods on this annotation visitor</i>.
+     * @param name the value name.
+     *
+     * @return a visitor to visit the actual array value elements, or <tt>null</tt> if this visitor is not interested in
+     *         visiting these values. The 'name' parameters passed to the methods of this visitor are ignored.
+     *         <i>All the array values must be visited before calling other methods on this annotation visitor</i>.
      */
     public AnnotationVisitor visitArray(String name) { return null; }
 
