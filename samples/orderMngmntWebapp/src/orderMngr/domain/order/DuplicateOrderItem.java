5640ce21189db242b88782d44a3bdf523fe85e8d
rliesenfeld <rliesenfeld@gmail.com>
2017-11-05 17:12:42 -0200

    Removed the "OrderManagement web app" sample module.

--- samples/orderMngmntWebapp/src/orderMngr/domain/order/DuplicateOrderItem.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2006 Rogério Liesenfeld
- * This file is subject to the terms of the MIT license (see LICENSE.txt).
- */
-package orderMngr.domain.order;
-
-public final class DuplicateOrderItem extends Exception
-{
-}
