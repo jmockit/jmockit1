895b086857697f4267f64045b6170dfa55fd7d2d
rliesenfeld <rliesenfeld@gmail.com>
2018-03-09 16:36:17 -0300

    Eliminated base class which only had one subclass left.

--- main/src/mockit/internal/expectations/mocking/InterfaceImplementationGenerator.java
+++ main/src/mockit/internal/expectations/mocking/InterfaceImplementationGenerator.java
@@ -5,22 +5,36 @@
 package mockit.internal.expectations.mocking;
 
 import java.lang.reflect.Type;
+import java.util.*;
 import javax.annotation.*;
 
+import static java.lang.reflect.Modifier.isStatic;
+
 import mockit.asm.*;
+import mockit.internal.*;
 import mockit.internal.classGeneration.*;
+import mockit.internal.reflection.*;
+import mockit.internal.reflection.GenericTypeReflection.*;
+import static mockit.asm.Access.*;
+import static mockit.asm.Opcodes.*;
 import static mockit.internal.expectations.mocking.MockedTypeModifier.*;
 
-final class InterfaceImplementationGenerator extends BaseImplementationGenerator
+final class InterfaceImplementationGenerator extends BaseClassModifier
 {
-   @Nonnull private final MockedTypeInfo mockedTypeInfo;
-   private String interfaceName;
+   private static final int CLASS_ACCESS = PUBLIC + FINAL;
 
-   InterfaceImplementationGenerator(
-      @Nonnull ClassReader classReader, @Nonnull Type mockedType, @Nonnull String implementationClassName
-   ) {
-      super(classReader, implementationClassName);
+   @Nonnull private final MockedTypeInfo mockedTypeInfo;
+   @Nonnull private final String implementationClassDesc;
+   @Nonnull private final List<String> implementedMethods;
+   private String interfaceName;
+   private String methodOwner;
+   @Nullable private String[] initialSuperInterfaces;
+
+   InterfaceImplementationGenerator(@Nonnull ClassReader classReader, @Nonnull Type mockedType, @Nonnull String implementationClassName) {
+      super(classReader);
       mockedTypeInfo = new MockedTypeInfo(mockedType);
+      implementationClassDesc = implementationClassName.replace('.', '/');
+      implementedMethods = new ArrayList<String>();
    }
 
    @Override
@@ -28,15 +42,57 @@ final class InterfaceImplementationGenerator extends BaseImplementationGenerator
       int version, int access, @Nonnull String name, @Nullable String signature, @Nullable String superName, @Nullable String[] interfaces
    ) {
       interfaceName = name;
+      methodOwner = name;
+      initialSuperInterfaces = interfaces;
+
       String classSignature = signature == null ? null : signature + mockedTypeInfo.implementationSignature;
-      super.visit(version, access, name, classSignature, superName, interfaces);
+      String[] implementedInterfaces = {name};
+      super.visit(version, CLASS_ACCESS, implementationClassDesc, classSignature, superName, implementedInterfaces);
+
+      generateNoArgsConstructor();
    }
 
-   @Override
-   protected void generateMethodBody(
+   private void generateNoArgsConstructor() {
+      mw = cw.visitMethod(PUBLIC, "<init>", "()V", null, null);
+      mw.visitVarInsn(ALOAD, 0);
+      mw.visitMethodInsn(INVOKESPECIAL, "java/lang/Object", "<init>", "()V", false);
+      generateEmptyImplementation();
+   }
+
+   @Override public AnnotationVisitor visitAnnotation(@Nonnull String desc) { return null; }
+   @Override public void visitInnerClass(@Nonnull String name, String outerName, String innerName, int access) {}
+   @Override public void visitOuterClass(@Nonnull String owner, @Nullable String name, @Nullable String desc) {}
+   @Override public void visitSource(@Nullable String source) {}
+
+   @Nullable @Override
+   public FieldVisitor visitField(
+      int access, @Nonnull String name, @Nonnull String desc, @Nullable String signature, @Nullable Object value) { return null; }
+
+   @Nullable @Override
+   public MethodVisitor visitMethod(
       int access, @Nonnull String name, @Nonnull String desc, @Nullable String signature, @Nullable String[] exceptions
    ) {
-      mw = cw.visitMethod(Access.PUBLIC, name, desc, signature, exceptions);
+      generateMethodImplementation(access, name, desc, signature, exceptions);
+      return null;
+   }
+
+   private void generateMethodImplementation(
+      int access, @Nonnull String name, @Nonnull String desc, @Nullable String signature, @Nullable String[] exceptions
+   ) {
+      if (!isStatic(access)) {
+         String methodNameAndDesc = name + desc;
+
+         if (!implementedMethods.contains(methodNameAndDesc)) {
+            generateMethodBody(access, name, desc, signature, exceptions);
+            implementedMethods.add(methodNameAndDesc);
+         }
+      }
+   }
+
+   private void generateMethodBody(
+      int access, @Nonnull String name, @Nonnull String desc, @Nullable String signature, @Nullable String[] exceptions
+   ) {
+      mw = cw.visitMethod(PUBLIC, name, desc, signature, exceptions);
 
       String className = null;
 
@@ -59,5 +115,82 @@ final class InterfaceImplementationGenerator extends BaseImplementationGenerator
       generateDirectCallToHandler(mw, className, access, name, desc, signature);
       generateReturnWithObjectAtTopOfTheStack(desc);
       mw.visitMaxStack(1);
+   }
+
+   @Nullable
+   private String getSubInterfaceOverride(
+      @Nonnull GenericTypeReflection genericTypeMap, @Nonnull String name, @Nonnull String genericSignature
+   ) {
+      if (!implementedMethods.isEmpty()) {
+         GenericSignature parsedSignature = genericTypeMap.parseSignature(genericSignature);
+
+         for (String implementedMethod : implementedMethods) {
+            if (sameMethodName(implementedMethod, name) && parsedSignature.satisfiesSignature(implementedMethod)) {
+               return implementedMethod;
+            }
+         }
+      }
+
+      return null;
+   }
+
+   private static boolean sameMethodName(@Nonnull String implementedMethod, @Nonnull String name) {
+      return implementedMethod.startsWith(name) && implementedMethod.charAt(name.length()) == '(';
+   }
+
+   private boolean isOverrideOfMethodFromSuperInterface(@Nonnull String name, @Nonnull String desc) {
+      if (!implementedMethods.isEmpty()) {
+         int p = desc.lastIndexOf(')');
+         String descNoReturnType = desc.substring(0, p + 1);
+
+         for (String implementedMethod : implementedMethods) {
+            if (sameMethodName(implementedMethod, name) && implementedMethod.contains(descNoReturnType)) {
+               return true;
+            }
+         }
+      }
+
+      return false;
+   }
+
+   @Override
+   public void visitEnd() {
+      assert initialSuperInterfaces != null;
+
+      for (String superInterface : initialSuperInterfaces) {
+         new MethodGeneratorForImplementedSuperInterface(superInterface);
+      }
+   }
+
+   private final class MethodGeneratorForImplementedSuperInterface extends ClassVisitor {
+      private String[] superInterfaces;
+
+      MethodGeneratorForImplementedSuperInterface(@Nonnull String interfaceName) {
+         ClassFile.visitClass(interfaceName, this);
+      }
+
+      @Override
+      public void visit(
+         int version, int access, @Nonnull String name, @Nullable String signature, @Nullable String superName,
+         @Nullable String[] interfaces
+      ) {
+         methodOwner = name;
+         superInterfaces = interfaces;
+      }
+
+      @Nullable @Override
+      public MethodVisitor visitMethod(
+         int access, @Nonnull String name, @Nonnull String desc, @Nullable String signature, @Nullable String[] exceptions
+      ) {
+         generateMethodImplementation(access, name, desc, signature, exceptions);
+         return null;
+      }
+
+      @Override
+      public void visitEnd() {
+         for (String superInterface : superInterfaces) {
+            new MethodGeneratorForImplementedSuperInterface(superInterface);
+         }
+      }
    }
 }
