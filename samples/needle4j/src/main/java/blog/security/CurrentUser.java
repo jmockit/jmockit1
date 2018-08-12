813a728d046a2f90907ee5450d8a2485fc7f429a
rliesenfeld <rliesenfeld@gmail.com>
2018-08-12 16:40:26 -0300

    Removed the "needle4j" sample test suite, as it's no longer interesting (having the "petclinic" sample is enough).

--- samples/needle4j/src/main/java/blog/security/CurrentUser.java
+++
@@ -1,9 +0,0 @@
-package blog.security;
-
-import java.lang.annotation.*;
-import javax.inject.*;
-
-@Qualifier
-@Target(ElementType.FIELD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface CurrentUser {}
