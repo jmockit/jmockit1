87baafbf08ddc15085e280b14490c6e89d2f5635
rliesenfeld <rliesenfeld@gmail.com>
2018-11-02 15:50:01 -0300

    Deprecated the Deencapsulation.setField methods, in favor of @Tested and @Injectable.

--- main/src/mockit/Deencapsulation.java
+++ main/src/mockit/Deencapsulation.java
@@ -12,7 +12,7 @@ import mockit.internal.reflection.*;
  * Provides utility methods that enable access to ("de-encapsulate") otherwise non-accessible fields.
  *
  * @see #getField(Object, String)
- * @see #setField(Object, String, Object)
+ * @see #getField(Object, Class)
  */
 public final class Deencapsulation
 {
@@ -29,7 +29,6 @@ public final class Deencapsulation
     *
     * @see #getField(Object, Class)
     * @see #getField(Class, String)
-    * @see #setField(Object, String, Object)
     */
    @Nullable
    public static <T> T getField(@Nonnull Object objectWithField, @Nonnull String fieldName) {
@@ -37,8 +36,8 @@ public final class Deencapsulation
    }
 
    /**
-    * Gets the value of a non-accessible (eg <tt>private</tt>) field from a given object, <em>assuming</em> there is only one field declared in the
-    * class of the given object whose type can receive values of the specified field type.
+    * Gets the value of a non-accessible (eg <tt>private</tt>) field from a given object, <em>assuming</em> there is only one field declared
+    * in the class of the given object whose type can receive values of the specified field type.
     *
     * @param objectWithField the instance from which to get the field value
     * @param fieldType the declared type of the field, or a sub-type of the declared field type
@@ -47,7 +46,6 @@ public final class Deencapsulation
     *
     * @see #getField(Object, String)
     * @see #getField(Class, String)
-    * @see #setField(Object, Object)
     */
    @Nullable
    public static <T> T getField(@Nonnull Object objectWithField, @Nonnull Class<T> fieldType) {
@@ -65,7 +63,6 @@ public final class Deencapsulation
     *
     * @see #getField(Class, Class)
     * @see #getField(Object, String)
-    * @see #setField(Class, String, Object)
     */
    @Nullable
    public static <T> T getField(@Nonnull Class<?> classWithStaticField, @Nonnull String fieldName) {
@@ -73,8 +70,8 @@ public final class Deencapsulation
    }
 
    /**
-    * Gets the value of a non-accessible static field defined in a given class, <em>assuming</em> there is only one field declared in the given class
-    * whose type can receive values of the specified field type.
+    * Gets the value of a non-accessible static field defined in a given class, <em>assuming</em> there is only one field declared in the
+    * given class whose type can receive values of the specified field type.
     *
     * @param classWithStaticField the class from which to get the field value
     * @param fieldType the declared type of the field, or a sub-type of the declared field type
@@ -84,7 +81,6 @@ public final class Deencapsulation
     *
     * @see #getField(Class, String)
     * @see #getField(Object, Class)
-    * @see #setField(Class, Object)
     */
    @Nullable
    public static <T> T getField(@Nonnull Class<?> classWithStaticField, @Nonnull Class<T> fieldType) {
@@ -100,10 +96,10 @@ public final class Deencapsulation
     *
     * @throws IllegalArgumentException if the desired field is not found
     *
-    * @see #setField(Class, String, Object)
-    * @see #setField(Object, Object)
-    * @see #getField(Object, String)
+    * @deprecated To inject mocked instances or regular values, use {@link Tested @Tested} or {@link Injectable @Injectable};
+    *             for other needs, use some other reflection utility library, or the Reflection API itself.
     */
+   @Deprecated
    public static void setField(@Nonnull Object objectWithField, @Nonnull String fieldName, @Nullable Object fieldValue) {
       FieldReflection.setField(objectWithField.getClass(), objectWithField, fieldName, fieldValue);
    }
@@ -114,10 +110,10 @@ public final class Deencapsulation
     *
     * @throws IllegalArgumentException if either the desired field is not found, or more than one is
     *
-    * @see #setField(Object, String, Object)
-    * @see #setField(Class, String, Object)
-    * @see #getField(Object, String)
+    * @deprecated To inject mocked instances or regular values, use {@link Tested @Tested} or {@link Injectable @Injectable};
+    *             for other needs, use some other reflection utility library, or the Reflection API itself.
     */
+   @Deprecated
    public static void setField(@Nonnull Object objectWithField, @Nonnull Object fieldValue) {
       FieldReflection.setField(objectWithField.getClass(), objectWithField, null, fieldValue);
    }
@@ -131,10 +127,9 @@ public final class Deencapsulation
     *
     * @throws IllegalArgumentException if the desired field is not found
     *
-    * @see #setField(Class, Object)
-    * @see #setField(Object, String, Object)
-    * @see #getField(Class, String)
+    * @deprecated If a <tt>static</tt> field truly needs to be set, use some other reflection utility library, or the Reflection API itself.
     */
+   @Deprecated
    public static void setField(@Nonnull Class<?> classWithStaticField, @Nonnull String fieldName, @Nullable Object fieldValue) {
       FieldReflection.setField(classWithStaticField, null, fieldName, fieldValue);
    }
@@ -148,10 +143,9 @@ public final class Deencapsulation
     *
     * @throws IllegalArgumentException if either the desired field is not found, or more than one is
     *
-    * @see #setField(Class, String, Object)
-    * @see #setField(Object, Object)
-    * @see #getField(Class, Class)
+    * @deprecated If a <tt>static</tt> field truly needs to be set, use some other reflection utility library, or the Reflection API itself.
     */
+   @Deprecated
    public static void setField(@Nonnull Class<?> classWithStaticField, @Nonnull Object fieldValue) {
       FieldReflection.setField(classWithStaticField, null, null, fieldValue);
    }
