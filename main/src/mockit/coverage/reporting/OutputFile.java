433a7f9400816abbac3ebe12dcd1d77e2af1cddf
rliesenfeld <rliesenfeld@gmail.com>
2019-08-20 23:32:30 -0300

    Coverage: separated coverage.css into two CSS files, for better control and efficiency.

--- main/src/mockit/coverage/reporting/OutputFile.java
+++ main/src/mockit/coverage/reporting/OutputFile.java
@@ -13,18 +13,18 @@ public final class OutputFile extends PrintWriter
    private static final Pattern PATH_SEPARATOR = Pattern.compile("/");
 
    @Nonnull private final String relPathToOutDir;
-   private final boolean withPrettyPrint;
+   private final boolean sourceFile;
 
    public OutputFile(@Nonnull File file) throws IOException {
       super(file, "UTF-8");
       relPathToOutDir = "";
-      withPrettyPrint = false;
+      sourceFile = false;
    }
 
    public OutputFile(@Nonnull String outputDir, @Nonnull String sourceFilePath) throws IOException {
       super(getOutputFileCreatingDirIfNeeded(outputDir, sourceFilePath));
       relPathToOutDir = getRelativeSubPathToOutputDir(sourceFilePath);
-      withPrettyPrint = true;
+      sourceFile = true;
    }
 
    @Nonnull
@@ -65,17 +65,17 @@ public final class OutputFile extends PrintWriter
       println("<html>");
       println("<head>");
       println("  <title>" + pageTitle + "</title>");
-      println("  <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'/>");
-      println("  <link rel='stylesheet' type='text/css' href='" + relPathToOutDir + "coverage.css'/>");
-      println("  <link rel='shortcut icon' type='image/png' href='" + relPathToOutDir + "logo.png'/>");
-      println("  <script type='text/javascript' src='" + relPathToOutDir + "coverage.js'></script>");
+      println("  <meta charset='UTF-8'>");
+      println("  <link rel='stylesheet' type='text/css' href='" + relPathToOutDir + (sourceFile ? "source" : "index") + ".css'>");
+      println("  <link rel='shortcut icon' type='image/png' href='" + relPathToOutDir + "logo.png'>");
+      println("  <script src='" + relPathToOutDir + "coverage.js'></script>");
 
-      if (withPrettyPrint) {
-         println("  <script type='text/javascript' src='" + relPathToOutDir + "prettify.js'></script>");
+      if (sourceFile) {
+         println("  <script src='" + relPathToOutDir + "prettify.js'></script>");
       }
 
       println("</head>");
-      println(withPrettyPrint ? "<body onload='prettyPrint()'>" : "<body>");
+      println(sourceFile ? "<body onload='prettyPrint()'>" : "<body>");
    }
 
    public void writeCommonFooter() {
