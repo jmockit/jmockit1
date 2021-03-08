c038bfec6751c2eac1cc9ea93c219b82f1a55285
José Fidalgo <jose.fidalgo@feedzai.com>
2021-03-08 16:52:10 +0000

    Added validation for classes that override methods from super

--- main/test/mockit/FakedClassWithSuperClassTest.java
+++ main/test/mockit/FakedClassWithSuperClassTest.java
@@ -5,17 +5,26 @@ import static org.junit.Assert.*;
 
 public final class FakedClassWithSuperClassTest
 {
-   static class BaseClass { protected int doSomething() { return 123; } }
-   public static class Subclass extends BaseClass { BaseClass getInstance() { return this; } }
+   public static class BaseClass {
+      protected int doSomething() { return 123; }
+      protected int doSomethingElse() { return 1; }
+   }
+
+   public static class Subclass extends BaseClass {
+      BaseClass getInstance() { return this; }
+      @Override protected int doSomethingElse() { return super.doSomethingElse() + 1; }
+   }
 
    public static final class FakeForSubclass extends MockUp<Subclass> {
       @Mock public int doSomething() { return 1; }
+      @Mock public int doSomethingElse(Invocation inv) { return inv.proceed(); }
    }
 
    @Test
    public void fakeOnlyInstancesOfTheClassSpecifiedToBeFaked() {
       BaseClass d = new Subclass();
       assertEquals(123, d.doSomething());
+      assertEquals(2, d.doSomethingElse());
 
       new FakeForSubclass();
 
@@ -24,15 +33,19 @@ public final class FakedClassWithSuperClassTest
       assertEquals(1, new Subclass().doSomething());
       assertEquals(123, new BaseClass() {}.doSomething());
       assertEquals(1, new Subclass() {}.doSomething());
+      assertEquals(1, new BaseClass().doSomethingElse());
+      assertEquals(2, new Subclass().doSomethingElse());
    }
 
    @Test
    public void fakeOnlyInstancesOfTheClassSpecifiedToBeFaked_usingFakeMethodBridge() {
       BaseClass d = new Subclass();
       assertEquals(123, d.doSomething());
+      assertEquals(2, d.doSomethingElse());
 
       new MockUp<Subclass>() {
          @Mock int doSomething() { return 2; }
+         @Mock public int doSomethingElse(Invocation inv) { return inv.proceed(); }
       };
 
       assertEquals(123, new BaseClass().doSomething());
@@ -40,5 +53,7 @@ public final class FakedClassWithSuperClassTest
       assertEquals(2, new Subclass().doSomething());
       assertEquals(123, new BaseClass() {}.doSomething());
       assertEquals(2, new Subclass() {}.doSomething());
+      assertEquals(1, new BaseClass().doSomethingElse());
+      assertEquals(2, new Subclass().doSomethingElse());
    }
 }
